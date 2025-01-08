f89c5ea141499b75bafef5ccc661607bcee80bf7
Caua-miguel <caua.sudol@outlook.com>
2025-01-07 21:04:06 -0300

    feat: url_prefix user

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from routes.user import user_route
 app = Flask(__name__)
 
 app.register_blueprint(home_route)
-app.register_blueprint(user_route)
+app.register_blueprint(user_route, url_prefix='/user')
 
 app.run(debug=True)
--- routes/user.py
+++ routes/user.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template
 
 user_route = Blueprint('user', __name__)
 
-@user_route.route('/user')
+@user_route.route('/')
 def home():
+    # Listar os usuários
+    return render_template('cadastro.html')
+
+@user_route.route('/new')
+def new_user():
     return render_template('cadastro.html')
