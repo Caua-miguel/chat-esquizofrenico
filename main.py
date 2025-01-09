5a02a4ab663542bb279b8f9cc312b43eb1818127
Caua-miguel <caua.sudol@outlook.com>
2025-01-08 21:39:38 -0300

    feat: chat template

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.home import home_route
 from routes.user import user_route
+from routes.chat import chat_route
 
 
 app = Flask(__name__)
 
 app.register_blueprint(home_route)
 app.register_blueprint(user_route, url_prefix='/user')
+app.register_blueprint(chat_route, url_prefix='/chat')
 
 app.run(debug=True)
