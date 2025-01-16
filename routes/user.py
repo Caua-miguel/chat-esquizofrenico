04102694ea2423e2efb5c653144fa55138e4ff69
Caua-miguel <caua.sudol@outlook.com>
2025-01-16 13:30:34 -0300

    feat: delete

--- routes/user.py
+++ routes/user.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, render_template
 from database.select import select_usuario
+from database.delete import delete_usuario
 
 user_route = Blueprint('user', __name__)
+usuarios = select_usuario()
 
 @user_route.route('/')
 def home():
-    usuarios = select_usuario()
     return render_template('lista_usuarios.html', usuarios=usuarios)
 
 @user_route.route('/new')
 def new_user():
     return render_template('cadastro.html')
+
+@user_route.route('/int:usuario.id/delete', methods=['DELETE'])
+def deletar_usuario(id):
+    delete_usuario(id)
