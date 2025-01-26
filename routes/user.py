6c83bfe1833023658612978370f5b2168e31e9cc
Caua-miguel <caua.sudol@outlook.com>
2025-01-26 18:11:50 -0300

    cadastro

--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from database.select import select_usuario, select_usuario_by_id
+from database.update import update_user_by_id
 from database.delete import delete_usuario
 
 user_route = Blueprint('user', __name__)
@@ -9,13 +10,25 @@ usuarios = select_usuario()
 def home():
     return render_template('lista_usuarios.html', usuarios=usuarios)
 
-@user_route.route('/new')
-def new_user():
-    return render_template('cadastro.html')
-
 @user_route.route('/<int:id>')
 def usuario_id(id):
     return render_template('lista_usuarios.html', usuario=select_usuario_by_id(id))
+
+@user_route.route('/cadastrar', methods=['POST'])
+def insert_user():
+    return render_template('cadastro.html')
+
+@user_route.route('editar/<int:id>', methods=['PUT'])
+def edit_user(id):
+    # data = request.get_json()
+
+    # nome = data["nome"]
+    # email = data["email"]
+    # status = data["status"]
+
+    # update_user_by_id(id, nome, email, status)
+
+    return render_template('cadastro.html', usuario=select_usuario_by_id(id))
 
 @user_route.route('/delete/<int:id>', methods=['POST'])
 def delete_user(id):
