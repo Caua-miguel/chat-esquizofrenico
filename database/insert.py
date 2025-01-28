a473c8b189d25dd843eb76fc5a2b8515858a4689
Caua-miguel <caua.sudol@outlook.com>
2025-01-27 22:25:32 -0300

    cadastro quebrado

--- database/insert.py
+++ database/insert.py
@@ -1,9 +1,9 @@
 from database.database import db, cursor
 
-def insert_user(nome, email, status):
+def insert_user(nome, email):
     cursor.execute(
         '''
         INSERT INTO usuarios (nome, email, status) VALUES (%s, %s, %s)
-        ''', (nome, email, status)
+        ''', (nome, email, True)
     )
     db.commit()
